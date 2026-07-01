f171392399860b04033965b634aa6eeb728fefdf
gothicsky <69666943+gothicsky@users.noreply.github.com>
2026-07-01 23:51:48 +0300

    indices
    a better way for managing multiple shapes without having redundant data

--- tutorial1.cpp
+++ tutorial1.cpp
@@ -13,14 +13,25 @@
 
 
 
-float triangleData[] = {
-  //positions  colors
-  //x, y,  z   r, g, b
-    0, 1, 0,   1, 0, 0, //vertex 1
-    -1, -1, 0, 0, 1, 0, //vertex 2
-    1, -1, 0,  0, 0, 1 //vertex 3
-};
+// float triangleData[] = {
+//   //positions  colors
+//   //x, y,  z   r, g, b
+//     0, 1, 0,   1, 1, 1, //vertex 1
+//     -1, -1, 0, 0, 0, 0, //vertex 2
+//     1, -1, 0,  0, 0, 0 //vertex 3
+// };
 
+
+float triangleData[] = {
+    //positions  colors
+    //x, y,  z   r, g, b
+    0.5, 0.5, 0,   1, 0, 0, //vertex 1
+    -0.5, 0.5, 0, 0, 1, 0, //vertex 2
+    -0.5, -0.5, 0,  0, 0, 1, //vertex 3
+    //second triangle
+    0.5, -0.5, 0, 0, 0, 1
+
+};
 
 // // different approach example 1
 // float triangleData[] = {
@@ -38,6 +49,12 @@ float triangleData[] = {
 // };
 
 
+unsigned short indices[] = {
+
+    0, 1, 2, //first triangle
+    0, 2, 3  //second triangle
+
+};
 
 
 
@@ -76,13 +93,14 @@ int main()
 
     enableReportGlErrors();
 
-    GLuint buffer = 0;
-
 
 #pragma region buffer
+
+    GLuint buffer = 0;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangleData), triangleData, GL_STATIC_DRAW);
+
 
 
     // // different approach example 1
@@ -102,7 +120,14 @@ int main()
 
 #pragma endregion
 
+#pragma region index buffer
 
+    GLuint iBuffer = 0;
+    glGenBuffers(1, &iBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+#pragma endregion
 
 #pragma region loadShader
 
@@ -114,14 +139,18 @@ int main()
 
 #pragma endregion
 
-
     while (!glfwWindowShouldClose(window))
     {
 
+        int w=0, h=0;
+        glfwGetWindowSize(window, &w, &h);
+        glViewport(0, 0, w, h);
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
