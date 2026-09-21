92a1dccd0e39dac89275ce6f2d5a91d6b94cdf3c
gothicsky <69666943+gothicsky@users.noreply.github.com>
2026-09-21 12:34:14 +0300

    seperated multiplications
    I will try to optimize this so uploading the latest working version just in case

--- math3d.h
+++ math3d.h
@@ -12,4 +12,18 @@ typedef struct {
 
 } Matrix4f;
 
+
+void matrix4f_init_usc(Matrix4f *mt, float coeff);
+void matrix4f_init_nusc(Matrix4f *mt, float x, float y, float z);
+
+
+//translation
 void matrix4f_init_translation(Matrix4f *mt, float x, float y, float z);
+
+//rotation
+void matrix4f_init_rot_x(Matrix4f *mt, float t);
+void matrix4f_init_rot_y(Matrix4f *mt, float t);
+void matrix4f_init_rot_z(Matrix4f *mt, float t);
+
+//Multiplication
+void mult_matrix4f(const Matrix4f *mt1, const Matrix4f *mt2, Matrix4f *res);
